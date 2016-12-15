30167bcb055dc782d58e3de961eddc4af4c8dd9c
The Thellere <thethellere@gmail.com>
2016-12-15 02:09:09 +0000

    Login fix attempt

---
+++ includes/dologin.php
@@ -0,0 +1,19 @@
+<?php
+session_start();
+$username = mysqli_escape_string($connection, $_POST['username']);
+$userResult = mysqli_query($connection, "SELECT * FROM `Users` WHERE `username` = '$username';");
+
+if(mysqli_num_rows($userResult) == 0){
+	echo '<div class="container"><div class="alert alert-warning" role="alert">User doesn\'t exist!</div></div><br>';
+} else {
+	$userRow = mysqli_fetch_array($userResult); //
+	$password = mysqli_escape_string($connection, $_POST['password']);
+	$correctPass = password_hash($password, $userRow['password']);
+	if ($correctPass == 1) {
+		echo '<div class="container"><div class="alert alert-success" role="alert">Login successful</div></div>';
+		$_SESSION['userID'] = $userRow['ID'];
+		$_SESSION['username'] = $userRow['username'];
+	} else { 
+	echo '<div class="container"><div class="alert alert-warning" role="alert">Error: Invalid username/password combination</div></div><br>';
+	}
+}
