8c14e29de9c0b76af801eae42ede777a88ad9ea1
The Thellere <thethellere@gmail.com>
2016-12-22 05:12:37 +0000

    Updates session creation to do diceroll to determine if a secondary type is added. Also adds species and basic character info form. No processing is done at this time

--- addsession.php
+++ addsession.php
@@ -3,22 +3,32 @@ session_start();
 require_once($_SERVER['DOCUMENT_ROOT'] . '/includes/database.php');
 require_once($_SERVER['DOCUMENT_ROOT'] . '/includes/accrow.php');
 
-function pickSessionType($userID) {
+function pickSessionType($userID) { //Randomly selects a session type. Provided the players has beaten a session, hardmode will be enabled. Note that this does not protect new players from joining one!
 	global $connection;
 	$userRow = mysqli_fetch_array(mysqli_query($connection, "SELECT * FROM `Users` WHERE `ID` = '$userID';"));
 	$hardmodeEnabled = $userRow['completedsession'];	//Is a boolean
 	if ($hardmodeEnabled == 0) {
-		$sessionTypePicked = mysqli_fetch_array(mysqli_query($connection, "SELECT * FROM `SessionTypes` WHERE `hardmode` = 0 AND `special` = 0 AND `ID` > 3 ORDER BY RAND() LIMIT 1;"));
+		$sessionTypePicked = mysqli_fetch_array(mysqli_query($connection, "SELECT * FROM `SessionTypes` WHERE `hardmode` = 0 AND `special` = 0 AND `ID` > 3 ORDER BY RAND() LIMIT 1;")); // Excludes hardmode. Ordering by RAND ensures that - as we're taking the top result - a random session type is picked from the resulting set.
 	} else {	
-		$sessionTypePicked = mysqli_fetch_array(mysqli_query($connection, "SELECT * FROM `SessionTypes` WHERE `special` = 0 AND `ID` > 3 ORDER BY RAND() LIMIT 1;"));
+		$sessionTypePicked = mysqli_fetch_array(mysqli_query($connection, "SELECT * FROM `SessionTypes` WHERE `special` = 0 AND `ID` > 3 ORDER BY RAND() LIMIT 1;")); // Above but hardmode is enabled.
 	}
-	$primaryType = rand(1,2); //Random primary type. Normal = 1, Null = 2
-	$typeString = $primaryType.'|'.$sessionTypePicked['ID'];
+	$primaryType = rand(1,2); //Random primary type. Normal = 1, Null = 2 
+  $chance = rand(0,100);
+  if ($hardmodeEnabled == 1) {
+    if ($chance <= 75) {
+      $typeString = $primaryType.'|'.$sessionTypePicked['ID'];
+    } else {
+      $typeString = $primaryType;
+    }
+  } else {
+    if ($chance <= 50) {
+      $typeString = $primaryType.'|'.$sessionTypePicked['ID'];
+    } else {
+      $typeString = $primaryType;
+    }
+  }
 	return $typeString;
 }
-
-
-
 
 $sessionName = mysqli_escape_string($connection, $_POST['sessionName']);
 $userCreating = $accountRow['ID'];
@@ -37,7 +47,7 @@ if ($sessionName == '' || $sessionName == null) {
     	$sessionType = pickSessionType($userCreating);
     	$hashedPass = password_hash($sessionPassword, PASSWORD_DEFAULT);
     	mysqli_query($connection, "INSERT INTO `Sessions` (`name`, `creator`, `password`, `type`) VALUES ('$sessionName', '$userCreating', '$hashedPass', '$sessionType');");
-    	echo '<div class="container"><div class="alert alert-success" role="alert">Success!</div></div>';
+    	echo '<div class="container"><div class="alert alert-success" role="alert">Success! Please make sure you\'re the first member of your session to ensure you get admin privileges.</div></div>';
     }
 }
 ?>
---
+++ charcreate.php
@@ -0,0 +1,49 @@
+<?php
+require_once($_SERVER['DOCUMENT_ROOT'] . '/includes/header.php');
+
+//Title
+?>
+<div class="container">
+	<h1>Create a Character!</h1>
+	<form role="form" class="form-horizontal" id="SessionForm" action="charcreate2.php" method="post">
+        <div class="form-group">
+            <label class="control-label col-sm-2" for="species">
+                <label class="radio-inline"><input type="radio" checked="checked" name="species" value="0">Human</label>
+				<label class="radio-inline"><input type="radio" name="species" value="1">Troll</label>
+            </div>
+        </div>
+        <div class="form-group">
+            <label class="control-label col-sm-2" for="characterName">Character Name: </label>
+            <div class="col-sm-3">
+                <input id="characterName" name="characterName" class="form-control" type="text" />
+            </div>
+        </div>
+        <div class="form-group">
+            <label class="control-label col-sm-2" for="chumHandle">Trollian/Pesterchum Handle: </label>
+            <div class="col-sm-3">
+                <input id="chumHandle" name="chumHandle" class="form-control" type="text" />
+            </div>
+        </div>
+        <div class="form-group">
+            <label class="control-label col-sm-2" for="sessionName">Session Name: </label>
+            <div class="col-sm-3">
+                <input id="sessionName" name="sessionName" class="form-control" type="text" />
+            </div>
+        </div>
+        <div class="form-group">
+            <label class="control-label col-sm-2" for="sessionPass">Session Password: </label>
+            <div class="col-sm-3">
+                <input id="sessionPass" name="sessionPass" class="form-control" maxlength="50" type="password" />
+            </div>
+        </div>
+        <div class="form-group">
+            <div class="col-sm-offset-2 col-sm-10">
+                <button name="submit" type="submit" class="btn btn-primary" id="submit" form="SessionForm">Next</button>
+            </div><br>
+        </div>
+
+<?php
+
+
+
+require_once($_SERVER['DOCUMENT_ROOT'] . '/includes/footer.php');
