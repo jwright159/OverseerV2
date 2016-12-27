596531a6369c8acdfbf577df0cb23518aa78ab33
Difarem <difarem@gmail.com>
2016-12-27 15:57:51 +0100

    typo

--- login.php
+++ login.php
@@ -23,7 +23,7 @@ require_once($_SERVER['DOCUMENT_ROOT'] . '/includes/header.php');
     <p>Login</p>
     <form role="form" class="form-horizontal" id="LoginForm" action="dolgin.php" method="post">
         <div class="form-group">
-            <label class="control-label col-sm-2" for="username">Userame: </label>
+            <label class="control-label col-sm-2" for="username">Username: </label>
             <div class="col-sm-3">
                 <input id="username" name="username" class="form-control" type="text" />
             </div>
