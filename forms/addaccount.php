b4a4c44c01be38485b94c336e25dbbb30cc32602
Erin <sylphofelectricity@gmail.com>
2017-01-05 20:19:24 -0600

    Port forms/addaccount.php to the ORM

--- forms/addaccount.php
+++ forms/addaccount.php
@@ -1,6 +1,11 @@
 <?php
+session_start();
+require_once __DIR__.'/../includes/bootstrap.php';
 
-require_once($_SERVER['DOCUMENT_ROOT'] . '/includes/database.php');
+use Overseer\Models\User;
+use Overseer\Models\UserQuery;
+
+
 function sendValidationEmail($mailto, $user, $confKey) {
 	$subject = 'Welcome to Overseer v2!';
 	$message = "Hello ".$user."!\n
@@ -20,12 +25,16 @@ function sendValidationEmail($mailto, $user, $confKey) {
 	mail($mailto, $subject, $message, $headers); 
 }
 
+function validateUsername($username) {
+	return (bool) preg_match('/^[\w_\-]+$/', $username) && (strlen($username) <= 32);
+}
+
 // Set the Post vars
-$username = mysqli_escape_string($connection, $_POST['username']);
-$email = mysqli_escape_string($connection, $_POST['email']);
-$emailConfirm = mysqli_escape_string($connection, $_POST['emailconf']);
-$password = mysqli_escape_string($connection, $_POST['password']);
-$passwordConfirm = mysqli_escape_string($connection, $_POST['confirmpw']);
+$username = $_POST['username'];
+$email = $_POST['email'];
+$emailConfirm = $_POST['emailconf'];
+$password = $_POST['password'];
+$passwordConfirm = $_POST['confirmpw'];
 
 // normalize email
 $email = strtolower($email);
@@ -34,24 +43,33 @@ $emailConfirm = strtolower($emailConfirm);
 if ($_POST['tos'] != "yes") {
 	echo '<div class="container"><div class="alert alert-danger" role="alert">You haven\'t accepted the conditions.</div></div>';
 } else {
-	$emailCheck = mysqli_query($connection, "SELECT `ID` FROM `Users` WHERE `email` = '$email';");
-	if (mysqli_num_rows($emailCheck) > 0) {
+	$emailCheck = UserQuery::create()->findOneByEmail($email);
+
+	if ($emailCheck) {
 		echo '<div class="container"><div class="alert alert-danger" role="alert">This email is already registered.</div></div>';
 	} elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
 		echo '<div class="container"><div class="alert alert-danger" role="alert">This email is invalid.</div></div>';
-	} elseif ($email != $emailConfirm || empty($email)) {
-		echo '<div class="container"><div class="alert alert-danger" role="alert">E-mails didn\'t match.</div></div>';
-	} elseif ($password != $passwordConfirm || empty($password)) {
+	} elseif ($email !== $emailConfirm || empty($email)) {
+		echo '<div class="container"><div class="alert alert-danger" role="alert">E-mails didn\'t match or were empty.</div></div>';
+	} elseif (!validateUsername($username)) {
+		echo '<div class="container"><div class="alert alert-danger" role="alert">Username is invalid!</div></div>';
+	} elseif ($password !== $passwordConfirm || empty($password)) {
 		echo '<div class="container"><div class="alert alert-danger" role="alert">Passwords didn\'t match.</div></div>';
 	} else {
-		$usernameCheck = mysqli_query($connection, "SELECT `ID` FROM `Users` WHERE `username` = '$username';");
-		if (mysqli_num_rows($usernameCheck) > 0) {
+		$usernameCheck = UserQuery::create()->findOneByUsername($username);
+
+		if ($usernameCheck) {
 			echo '<div class="container"><div class="alert alert-danger" role="alert">Username taken!</div></div>';
 		} else {
 			$confirmationKey = substr(md5(rand()), 0, 20);
 			$hashedPass = password_hash($password, PASSWORD_DEFAULT);
-			$query = "INSERT INTO `Users` (`username`, `email`, `password`, `confirmationkey`) VALUES ('$username', '$email', '$hashedPass', '$confirmationKey');";
-			mysqli_query($connection, $query);
+
+			// create and store a new user in the db
+			$newUser = new User();
+			$newUser->fromArray(["Username" => $username, "Password" => $hashedPass, "Email" => $email,
+				"Confirmed" => false, "ConfirmationKey" => $confirmationKey]);
+			$newUser->save();
+
 			sendValidationEmail($email, $username, $confirmationKey);
 			echo '<div class="container"><div class="alert alert-success" role="alert">Success! Check your email for a validation key.</div></div>';
 		}
