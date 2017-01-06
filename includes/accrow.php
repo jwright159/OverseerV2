7a750ebcc3e75a02e5886d33747bbc5be77fe3e0
Erin <sylphofelectricity@gmail.com>
2017-01-05 18:44:15 -0600

    accrow.php: make $_SESSION properties camelcase

--- includes/accrow.php
+++ includes/accrow.php
@@ -7,24 +7,23 @@
 use Overseer\Models\CharacterQuery;
 use Overseer\Models\UserQuery;
 
-if (!empty($_SESSION['user_id'])) {
-	$uid = $_SESSION['user_id'];
+if (!empty($_SESSION['userId'])) {
+	$uid = $_SESSION['userId'];
 
 	// fetch the user object from the db
 	$user = UserQuery::create()->findPK($uid); // findPK = find by primary key = find by id (since User's pk = `id`)
 	$_SESSION['username'] = $user->getUsername();
 
-	if (!empty($_SESSION['character_id'])) {
-		$cid = $_SESSION['character_id'];
+	if (!empty($_SESSION['characterId'])) {
+		$cid = $_SESSION['characterId'];
 
 		$character = CharacterQuery::create()->findPK($cid);
 		if ($character->getOwner() !== $user) {
 			// TODO: replace with a real error flash
 			echo "ERROR: You tried to select a character that doesn't belong to you!";
 
-			// wipe the character out of the request and session
-			unset($_SESSION['character']);
-			unset($character);
+			unset($_SESSION['characterId']);
+			unset($currentCharacter);
 		}
 	}
 }
