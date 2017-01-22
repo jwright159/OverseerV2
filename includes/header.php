54acc439614847645ca5fe08d235620b8e6b1a5b
Difarem <difarem@gmail.com>
2017-01-22 15:08:46 +0100

    header html paths are now absolute

--- includes/header.php
+++ includes/header.php
@@ -63,9 +63,9 @@ if ($maint != 0 && $accountRow['modlevel'] < 99 && !stripos($_SERVER['REQUEST_UR
 		<title><?php echo $title; ?></title>
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<link rel="stylesheet" href="includes/css/bootstrap.min.css">
+		<link rel="stylesheet" href="/includes/css/bootstrap.min.css">
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-		<script src="includes/js/bootstrap.min.js"></script>
+		<script src="/includes/js/bootstrap.min.js"></script>
 	</head>
 
 		<body>
