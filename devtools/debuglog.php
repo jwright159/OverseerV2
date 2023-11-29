82cb292c2437eb577dd7f1337fe74ba9f861efbf
Jady Wright <iamjwright159@gmail.com>
2023-11-29 14:32:54 -0500

    Add cwd to debuglog error message

--- devtools/debuglog.php
+++ devtools/debuglog.php
@@ -8,12 +8,13 @@ if ($accrow['modlevel'] < 10) {
     clearstatcache(); 
   }
   echo "Have a debug log.<br />";
-  if (!file_exists("debuglog.txt")) {
+  if (!is_writable(".")) {
+	$cwd = getcwd();
     echo "<br />";
-    echo "The debug log hasn't been created yet! (This may be due to permission problems)<br />";
-  } elseif (!is_writable("debuglog.txt")) {
+    echo "The debug log directory ($cwd) isn't writable!<br />";
+  } elseif (!file_exists("debuglog.txt")) {
     echo "<br />";
-    echo "The debug log isn't writable!<br />";
+    echo "The debug log hasn't been created yet!<br />";
   } elseif (filesize("debuglog.txt") === 0) {
     echo "<br />";
     echo "The debug log is empty!<br />";
