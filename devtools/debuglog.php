b40084bd3ee449b4ad778f524855369a48229bd8
Jady Wright <iamjwright159@gmail.com>
2023-11-29 15:29:40 -0500

    Fix debuglog key warning

--- devtools/debuglog.php
+++ devtools/debuglog.php
@@ -4,7 +4,7 @@ if ($accrow['modlevel'] < 10) {
   echo "You don't have sufficient permissions to view the debug log!";
  } else {
   $filepath = dirname(__FILE__) . "/debuglog.txt";
-  if ($_POST['clear']) {
+  if (isset($_POST['clear']) AND $_POST['clear'] == true) {
     unlink($filepath);
     clearstatcache(); 
   }
