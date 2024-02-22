cf5c99b156f4405a67dab6683fbfd897b604c776
Jady Wright <iamjwright159@gmail.com>
2024-02-22 16:03:34 -0500

    Remove enterance restriction from dungeonexit

--- dungeonexit.php
+++ dungeonexit.php
@@ -6,11 +6,10 @@ if (empty($_SESSION['character'])) {
 	echo "Select a character to go dungeon diving.<br />";
 } elseif ($charrow['dungeon'] == 0) {
 	echo "You are not currently in a dungeon.<br />";
-} elseif ($charrow['dungeonrow'] != 0 || $charrow['dungeoncol'] != 0) {
-	echo "You are not at the dungeon entrance! You can't exit it right now.<br />";
+// } elseif ($charrow['dungeonrow'] != 0 || $charrow['dungeoncol'] != 0) {
+//	echo "You are not at the dungeon entrance! You can't exit it right now.<br />";
 } else {
 	echo "You exit the dungeon.<br />";
 	mysqli_query($connection, "UPDATE Characters SET dungeon = 0 WHERE Characters.ID = " . $charrow['ID'] . " LIMIT 1;");
 }
 require_once "footer.php";
-?>
